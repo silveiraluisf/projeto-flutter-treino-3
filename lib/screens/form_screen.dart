1b52a3bd85d21d2d5be70658b48d52fd8c0ee687
silveiraluisf <silveiraluis88@gmail.com>
2022-09-19 16:45:04 -0300

    primeiro campo do formulário adicionado

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -12,7 +12,7 @@ class _FormScreenState extends State<FormScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Formulário'),
+        title: const Text('New Activity'),
       ),
       body: Center(
         child: Container(
@@ -22,6 +22,21 @@ class _FormScreenState extends State<FormScreen> {
             color: Colors.black12,
             borderRadius: BorderRadius.circular(10),
             border: Border.all(width: 3),
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Name',
+                    fillColor: Colors.white70,
+                    filled: true,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
