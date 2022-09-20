e7dccd18571c8dbeee56c37b631c068204aab4e2
silveiraluisf <silveiraluis88@gmail.com>
2022-09-20 09:28:46 -0300

    adicionado containter para mostrar imagem

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -58,6 +58,11 @@ class _FormScreenState extends State<FormScreen> {
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
+                    onChanged: (text){
+                      setState(() {
+
+                      });
+                    },
                     controller: imageController,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
@@ -65,6 +70,19 @@ class _FormScreenState extends State<FormScreen> {
                       fillColor: Colors.white70,
                       filled: true,
                     ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 72,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(width: 2, color: Colors.blue),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(imageController.text, fit: BoxFit.cover,),
                   ),
                 ),
                 ElevatedButton(
