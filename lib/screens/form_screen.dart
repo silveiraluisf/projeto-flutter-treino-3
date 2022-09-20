afb40962da052ff811bf83086c293e51c56f231b
silveiraluisf <silveiraluis88@gmail.com>
2022-09-20 07:55:25 -0300

    adicionado os outros campos do formulario junto com seus respectivos controllers

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -19,60 +19,63 @@ class _FormScreenState extends State<FormScreen> {
         title: const Text('New Activity'),
       ),
       body: Center(
-        child: Container(
-          height: 550,
-          width: 375,
-          decoration: BoxDecoration(
-            color: Colors.black12,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(width: 3),
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Name',
-                    fillColor: Colors.white70,
-                    filled: true,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 550,
+            width: 375,
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(width: 3),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Name',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: difficultyController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Difficulty',
-                    fillColor: Colors.white70,
-                    filled: true,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: difficultyController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Difficulty',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: imageController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Image',
-                    fillColor: Colors.white70,
-                    filled: true,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: imageController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Image',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
                 ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print(nameController.text);
-                    print(int.parse(difficultyController.text));
-                    print(imageController.text);
-                  },
-                  child: const Text('Add'))
-            ],
+                ElevatedButton(
+                    onPressed: () {
+                      print(nameController.text);
+                      print(int.parse(difficultyController.text));
+                      print(imageController.text);
+                    },
+                    child: const Text('Add'))
+              ],
+            ),
           ),
         ),
       ),
